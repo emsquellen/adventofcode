950e809cfc6a779c7767976d0ddb844a27ae3d11
emsquellen <24551188+emsquellen@users.noreply.github.com>
2021-12-04 22:54:10 +0100

    Day 3, part 1

---
+++ day_3/binary_diagnostic.py
@@ -0,0 +1,2 @@
+def part_1():
+    return d * (d ^ 0b111111111111) if (d := int("".join([str(int(i)) for i in [1000 - sum(x) < sum(x) for x in list(zip(*[[int(x) for x in str(ln.strip())] for ln in open("input3.txt", "r").readlines()]))]]), 2)) else 0
